c4ac6c7c2b8d63181a436c3a3aec31c143c62d10
Dinh Long Nguyen <dinhlongviolin1@gmail.com>
2019-12-31 18:01:12 +0700

    Serial Port constant var

--- raspberry pi/server.py
+++ raspberry pi/server.py
@@ -4,10 +4,12 @@ import requests
 import json
 from gpiozero import LED
 
+SERIAL_PORT = 9600
+
 API_endpoint = "http://3.1.194.44:3000/api/sensor"
 
 led = LED(17)
-ser = serial.Serial('/dev/ttyACM0', 9600)
+ser = serial.Serial('/dev/ttyACM0', SERIAL_PORT)
 time.sleep(2)
 
 while True:
